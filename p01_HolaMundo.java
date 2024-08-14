27dbefaac73bfde34dfb3d25143e962e2c3f5f48
maxsteel <max_monla09@gmail.com>
2024-08-14 09:06:48 -0600

    primero

---
+++ p01_HolaMundo.java
@@ -0,0 +1,8 @@
+/**
+ * p01_HolaMundo - Manda una salida a la pantalla
+ */
+public class p01_HolaMundo {
+    public static void main(string[] args) {
+        System.out.prinln("Hola Mundo en el lengiaje java");
+    }
+}
---
+++ p03_AreaTriangulo.java
@@ -0,0 +1,19 @@
+//p03_AreaTriangulo -Calcula el area de un triangulo
+
+import java.util.Scanner;
+
+public class p03_AreaTriangulo {
+    public static void main(string[] args) {
+        int base, altura;
+        double area;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("\nCalculando el area de un triangulo:\n");
+        System.out.println("Dame la base ? ");
+        base = obj.nextInt();
+        System.out.println("Dame la altura ? ");
+        altura = obj.nextInt();
+        
+        area = ( base = altura ) / 2;
+
+        System.out.println("Para")
