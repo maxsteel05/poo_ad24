a7821bed900da8f12148d7eb7b82f321902e39d6
maxsteel05 <max.monla09@gmail.com>
2024-08-15 22:55:26 -0600

    Update p02_AreaCirculo.java

--- p02_AreaCirculo.java
+++ p02_AreaCirculo.java
@@ -1,3 +1,20 @@
+// p02_AreaCirculo - calcular el área de un círculo
+
+import java.util.Scanner;
+
 public class p02_AreaCirculo {
-    
+
+    public static void main(String[] args) {
+        double radio, area;
+        Scanner lradio = new Scanner(System.in);
+
+        System.out.println("\nCalculando el área de un círculo\n");
+        System.out.print("Dame el radio del círculo: ");
+        radio = lradio.nextDouble();  
+
+        area = Math.PI * Math.pow(radio, 2); 
+
+        System.out.println("\nEl círculo con radio de " + radio + " tiene un área de " + area);
+    }
 }
+
