1ab9d579ad80cedad2cf65533d561ad9ea6a4505
maxsteel05 <max.monla09@gmail.com>
2024-08-15 22:54:02 -0600

    Update p01_HolaMundo.java

--- p01_HolaMundo.java
+++ p01_HolaMundo.java
@@ -1,8 +1,14 @@
-/**
- * p01_HolaMundo - Manda una salida a la pantalla
- */
+// p01_HolaMundo - Manda una salida a la pantalla
+
 public class p01_HolaMundo {
-    public static void main(string[] args) {
-        System.out.prinln("Hola Mundo en el lengiaje java");
+    public static void main(String[] args) { 
+        String amigo = "Adrian";
+        String hermano = "Reynaldo";
+        String mensaje = String.format("Tanto %s como %s desean aprender Java", amigo, hermano);  
+
+        System.out.println("Hola Mundo en el lenguaje Java");  
+        System.out.println("\nHola amigo " + amigo + " Bienvenido a Java"); 
+        System.out.println("\nMi amigo " + amigo + " Mi hermano es " + hermano);  
+        System.out.println("\n" + mensaje);  
     }
 }
