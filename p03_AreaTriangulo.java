3b76ac8625d9bd8aaf64aedaee410ec2a3ee5c65
maxsteel05 <max.monla09@gmail.com>
2024-08-15 22:56:33 -0600

    Update p03_AreaTriangulo.java

--- p03_AreaTriangulo.java
+++ p03_AreaTriangulo.java
@@ -1,19 +1,21 @@
-//p03_AreaTriangulo -Calcula el area de un triangulo
+// p03_AreaTriangulo - Calcula el área de un triángulo
 
 import java.util.Scanner;
 
 public class p03_AreaTriangulo {
-    public static void main(string[] args) {
+    public static void main(String[] args) {  
         int base, altura;
         double area;
         Scanner obj = new Scanner(System.in);
 
-        System.out.println("\nCalculando el area de un triangulo:\n");
-        System.out.println("Dame la base ? ");
+        System.out.println("\nCalculando el área de un triángulo:\n");
+        System.out.println("Dame la base?");
         base = obj.nextInt();
-        System.out.println("Dame la altura ? ");
+        System.out.println("Dame la altura?");
         altura = obj.nextInt();
         
-        area = ( base = altura ) / 2;
+        area = (double)(base * altura) / 2;  
 
-        System.out.println("Para")
+        System.out.println(String.format("El triángulo de base %d y altura %d tiene un área de %.2f", base, altura, area));  
+    }
+}
