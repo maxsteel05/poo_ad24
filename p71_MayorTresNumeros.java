6f1a29996336849aa3e260eb71f23ae93188e2a7
maxsteel05 <max.monla09@gmail.com>
2024-09-18 23:13:04 -0600

    Update p71_MayorTresNumeros.java

--- p71_MayorTresNumeros.java
+++ p71_MayorTresNumeros.java
@@ -1,1 +1,24 @@
 
+
+import java.util.Scanner;
+
+public class p71_MayorTresNumeros {
+
+    public static void Tabla(int tabla, int limite) {
+        for(int i = 1; i<=limite; i++) {
+            System.out.printf("%d x %d = %d \n", tabla, i, tabla * i);
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        Scanner Max = new Scanner(System.in);
+
+        System.out.println("Que tabla quieres ? "); int tabla = Max.nextInt();
+        System.out.println("Hasta dónde       ? "); int limite = Max.nextInt();
+
+        Tabla(tabla, limite);
+        Max.close();
+    }
+}
