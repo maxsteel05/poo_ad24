4d37131ab4e1a806e20d401b338b20e108f5f18d
maxsteel05 <max.monla09@gmail.com>
2024-10-01 23:20:55 -0600

    Create Circulo.java

---
+++ Práctica_08_p98_Circulo/Circulo.java
@@ -0,0 +1,30 @@
+package Práctica_08_p98_Circulo;
+
+public class Circulo {
+    private double Radio;
+ 
+
+    public Circulo() {}
+    public Circulo(double radio) {
+        Radio = radio;
+    }
+
+
+    public double getRadio() {
+        return Radio;
+    }
+    public void setRadio(double radio) {
+        Radio = radio;
+    }
+
+    public double getArea() {
+        return (Radio * Radio * Math.PI);
+    }
+    public double getCircunferencia() {
+        return (2 * Math.PI * Radio);
+    }
+    @Override
+    public String toString() {
+        return "Circulo [Radio=" + Radio + "]";
+    }
+}
