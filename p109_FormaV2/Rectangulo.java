30f708cb645d758f219952228d15b08b990ec25e
maxsteel05 <max.monla09@gmail.com>
2024-10-09 08:42:57 -0600

    Create Rectangulo.java

---
+++ p109_FormaV2/Rectangulo.java
@@ -0,0 +1,46 @@
+package p109_FormaV2;
+
+public class Rectangulo extends Forma {
+    private double largo;
+    private double ancho;
+
+    public Rectangulo() {
+    }
+
+    public Rectangulo(String color, boolean relleno, double largo, double ancho) {
+        super(color, relleno);  // Llama al constructor de la clase base
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    @Override
+    public double getArea() {
+        return largo * ancho;
+    }
+
+    @Override
+    public double getPerimetro() {
+        return 2 * (largo + ancho);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangulo [" + super.toString() + ", Largo=" + largo + ", Ancho=" + ancho + "]";
+    }
+}
