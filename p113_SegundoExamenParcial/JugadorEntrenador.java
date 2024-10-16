6a233dc0ec6bafba9d9138c5488351c411b493d9
maxsteel05 <max.monla09@gmail.com>
2024-10-16 09:29:14 -0600

    Update JugadorEntrenador.java

--- p113_SegundoExamenParcial/JugadorEntrenador.java
+++ p113_SegundoExamenParcial/JugadorEntrenador.java
@@ -1,26 +1,24 @@
-public class JugadorEntrenador extends Jugador {
-    private int subordinados;
-    private int proyectos;
+package p113_SegundoExamenParcial;
 
-    public JugadorEntrenador(String nombre, char sexo, String descripcion, double salario, int subordinados, int proyectos) {
-        super(nombre, sexo, descripcion, salario);
-        this.subordinados = subordinados;
-        this.proyectos = proyectos;
-        this.total = salario + getBono();
+public class JugadorEntrenador extends Deportista {
+    private int personalACargo;
+    private int proyectosDirigidos;
+
+    public JugadorEntrenador(String nombreCompleto, char genero, String detallesAdicionales, double remuneracion, int personalACargo, int proyectosDirigidos) {
+        super(nombreCompleto, genero, detallesAdicionales, remuneracion);
+        this.personalACargo = personalACargo;
+        this.proyectosDirigidos = proyectosDirigidos;
+        this.ingresosFinales = remuneracion + calcularBono(); 
     }
-
     @Override
-    public double getBono() {
-        return (getSalario() * 0.15) + (proyectos * 100) + (subordinados * 10);
+    public double calcularBono() {
+        return (obtenerRemuneracion() * 0.15) + (proyectosDirigidos * 100) + (personalACargo * 10);
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", Subordinados=" + subordinados + 
-               ", Proyectos=" + proyectos + ", Bono=" + getBono() + ", Total=" + total;
-    }
-
-    public double getSalario() {
-        return super.salario;
+        return "JugadorEntrenador [" + super.toString() + ", Personal a Cargo=" + personalACargo + ", Proyectos Dirigidos=" + proyectosDirigidos + 
+               ", Bono Obtenido=" + calcularBono() + ", Ingresos Finales=" + ingresosFinales + "]";
     }
 }
+
