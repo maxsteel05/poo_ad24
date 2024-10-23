d4e636d29991f3ebfa255accb281433a4104fcb9
maxsteel05 <max.monla09@gmail.com>
2024-10-22 22:34:41 -0600

    Create Gato.java

---
+++ p114_Animal/Gato.java
@@ -0,0 +1,17 @@
+package p114_Animal;
+
+public class Gato implements Animal {
+
+    @Override
+    public void dormir() {
+        System.out.println("El gato duerme en una caja");
+        
+    }
+
+    @Override
+    public void sonido() {
+        System.out.println("El gato hace miauauauauauaua");
+        
+    }
+    
+}
