8a653a553a5e28e7173e43e959d1b1070fddf8c0
maxsteel05 <max.monla09@gmail.com>
2024-10-24 00:14:08 -0600

    Create Organismo.java

---
+++ p119_Organismo/Organismo.java
@@ -0,0 +1,25 @@
+package p119_Organismo;
+
+public class Organismo {
+    private String Nombre;
+
+    public Organismo(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void respiración() {
+        System.out.println("Respirando ... ");
+    }
+
+    public void movimiento() {
+        System.out.println("Moviendose ... ");
+    }
+
+    public void crecimiento() {
+        System.out.println("Creciendo ... ");
+    }
+}
