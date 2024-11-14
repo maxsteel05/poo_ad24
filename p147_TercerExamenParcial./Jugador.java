8156fcd4a0c92ddd419f3acc1f3207031d0085bd
maxsteel05 <max.monla09@gmail.com>
2024-11-14 09:40:26 -0600

    Update Jugador.java

--- p147_TercerExamenParcial./Jugador.java
+++ p147_TercerExamenParcial./Jugador.java
@@ -3,34 +3,78 @@ package p147_TercerExamenParcial;
 import java.io.Serializable;
 
 public class Jugador implements Serializable {
-    private String nombre;
-    private int edad;
-    private char sexo;
-    private String estadoCivil;
-    private String descripcion;
-    private double salario;
+
+    
+    private String nombre; // Nombre completo
+    private int edad; 
+    private char sexo; // Sexo ('M' o 'F')
+    private String estadocivil; 
+    private String descripcion; // Puesto o rol en el equipo
+    private double salario; // Salario mensual
 
     public Jugador() {}
 
-    public Jugador(String nombre, int edad, char sexo, String estadoCivil, String descripcion, double salario) {
+    public Jugador(String nombre, int edad, char sexo, String estadocivil, String descripcion, double salario) {
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
-        this.estadoCivil = estadoCivil;
+        this.estadocivil = estadocivil;
         this.descripcion = descripcion;
         this.salario = salario;
     }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public int getEdad() { return edad; }
-    public void setEdad(int edad) { this.edad = edad; }
-    public char getSexo() { return sexo; }
-    public void setSexo(char sexo) { this.sexo = sexo; }
-    public String getEstadoCivil() { return estadoCivil; }
-    public void setEstadoCivil(String estadoCivil) { this.estadoCivil = estadoCivil; }
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-    public double getSalario() { return salario; }
-    public void setSalario(double salario) { this.salario = salario; }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadocivil() {
+        return estadocivil;
+    }
+    public void setEstadocivil(String estadocivil) {
+        this.estadocivil = estadocivil;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", sexo=" + sexo +
+                ", estadocivil='" + estadocivil + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
 }
+
